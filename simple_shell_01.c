37dc8c5519afca65b4bbe081841f3af7f33d9b12
ShAlabbadi <10708@holbertonstudents.com>
2025-04-17 13:39:30 +0000

    test

--- simple_shell_01.c
+++ simple_shell_01.c
@@ -1,17 +1,12 @@
 #include "shell.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 /**
  * main - Simple shell entry point
  * @argc: Argument count (unused)
  * @argv: Argument vector (unused)
  * @env: Environment variables
- *
  * Return: EXIT_SUCCESS on success
  */
-
 int main(int argc __attribute__((unused)),
 char **argv __attribute__((unused)),
 char **env)
@@ -19,8 +14,6 @@ char **env)
 char *input;
 char *args[MAX_ARGS];
 int interactive = isatty(STDIN_FILENO);
-char *command;
-char *newline_pos;
 
 while (1)
 {
@@ -33,20 +26,8 @@ printf("\n");
 break;
 }
 
-command = input;
-while ((newline_pos = strchr(command, '\n')) != NULL)
-{
-*newline_pos = '\0';
-if (parse_input(command, args) > 0)
+if (parse_input(input, args) > 0)
 execute_command(args, env);
-command = newline_pos + 1;
-}
-
-if (*command != '\0')
-{
-if (parse_input(command, args) > 0)
-execute_command(args, env);
-}
 }
 return (EXIT_SUCCESS);
 }
