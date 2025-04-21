d281bead215344a48b17c42ec3b23cd960674f2a
ShAlabbadi <10708@holbertonstudents.com>
2025-04-21 08:38:52 +0000

    test28

--- executor.c
+++ executor.c
@@ -11,31 +11,31 @@
  */
 int execute_command(char **args, char **env)
 {
-    pid_t pid;
-    int status;
+pid_t pid;
+int status;
 
-    if (access(args[0], X_OK) == -1)
-    {
-        perror(args[0]);
-        return (0);
-    }
+if (access(args[0], X_OK) == -1)
+{
+perror(args[0]);
+return (0);
+}
 
-    pid = fork();
-    if (pid == -1)
-    {
-        perror("fork");
-        return (0);
-    }
+pid = fork();
+if (pid == -1)
+{
+perror("fork");
+return (0);
+}
 
-    if (pid == 0)
-    {
-        execve(args[0], args, env);
-        perror(args[0]);
-        exit(EXIT_FAILURE);
-    }
-    else
-    {
-        waitpid(pid, &status, 0);
-    }
-    return (1);
+if (pid == 0)
+{
+execve(args[0], args, env);
+perror(args[0]);
+exit(EXIT_FAILURE);
+}
+else
+{
+waitpid(pid, &status, 0);
+}
+return (1);
 }
