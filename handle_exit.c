489a7ac0832cbd97fb495059dc62c2c694a241f8
ShAlabbadi <10708@holbertonstudents.com>
2025-04-23 12:36:10 +0000

    s

--- handle_exit.c
+++ handle_exit.c
@@ -68,28 +68,27 @@ int execute_command(char **args, int *last_status, int *cmd_count)
 
     pid = fork();
     if (pid == 0)
-    {/*new*/
-        /* Check if PATH is empty */
+   {
+  /*      
         char *path = getenv("PATH");
         if (path == NULL || path[0] == '\0')
         {
             fprintf(stderr, "./hsh: %d: %s: not found\n", *cmd_count, args[0]);
             exit(127);
         }
-    /* Check if PATH exists at all */
+    
         if (getenv("PATH") == NULL)
         {
             fprintf(stderr, "./hsh: %d: %s: not found\n", *cmd_count, args[0]);
             exit(127);
         }
-	    
+*/	    
         execvp(args[0], args);
         fprintf(stderr, "./hsh: %d: %s: not found\n", *cmd_count, args[0]);
         exit(127);
     }
     else if (pid > 0)
     {
-       (*cmd_count)++;
         waitpid(pid, &status, 0);
         *last_status = WEXITSTATUS(status);
         return *last_status;
