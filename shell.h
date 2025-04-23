0f500e12cbea76344a10814f611f0692e951ea55
ShAlabbadi <10708@holbertonstudents.com>
2025-04-23 09:46:16 +0000

    a

--- shell.h
+++ shell.h
@@ -7,7 +7,6 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
-#include <unistd.h>  /*new*/
 
 
 
@@ -20,10 +19,8 @@ char *_strchr(const char *s, int c);
 char *_strtok(char *str, const char *delim);
 int _strcmp(char *s1, char *s2);
 int _atoi(char *s);
-
-/*int handle_exit(char **args);
-int execute_command(char **args);*/
-
-void handle_exit(char **args, int last_status);/*new*/
+void handle_exit(char **args, int last_status);
 int execute_command(char **args, int *last_status);
+
+
 #endif
