42b01a47a5d90b3a2697091dd02593cfa6b2a920
muniira-03 <munirra.alsubaie@gmail.com>
2025-04-24 12:14:33 +0000

    simple shell

--- simple_shell.c
+++ simple_shell.c
@@ -1,68 +1,68 @@
 #include "shell.h"
 
 /**
- * main - Simple shell with proper command processing
- * Return: Exit status
- */
+	* main - Simple shell with proper command processing
+	* Return: Exit status
+	*/
 
 int main(int argc, char **argv, char **env)
 {
-    char input[MAX_INPUT] = {0};
-    char *commands[MAX_INPUT/2 + 1] = {NULL};
-    char *args[MAX_ARGS] = {NULL};
-    int interactive = isatty(STDIN_FILENO);
-    ssize_t read_bytes;
-    int i, j, cmd_count;
-    char *cmd_token, *arg_token;
-      int last_status = 0;
- int cmd_counter = 1;
+	char input[MAX_INPUT] = {0};
+	char *commands[MAX_INPUT/2 + 1] = {NULL};
+	char *args[MAX_ARGS] = {NULL};
+	int interactive = isatty(STDIN_FILENO);
+	ssize_t read_bytes;
+	int i, j, cmd_count;
+	char *cmd_token, *arg_token;
+	int last_status = 0;
+	int cmd_counter = 1;
 (void)argc;
-    (void)argv;
+	(void)argv;
 
 
-    while (1)
-    {
-        if (interactive)
-            write(STDOUT_FILENO, "$ ", 2);
+	while (1)
+	{
+	if (interactive)
+	write(STDOUT_FILENO, "$ ", 2);
 
-        read_bytes = read(STDIN_FILENO, input, MAX_INPUT - 1);
-        if (read_bytes <= 0)
-            break;
+	read_bytes = read(STDIN_FILENO, input, MAX_INPUT - 1);
+	if (read_bytes <= 0)
+	break;
 
-        input[read_bytes] = '\0';
+	input[read_bytes] = '\0';
 
-        /* Split into commands (by newline) */
-        cmd_count = 0;
-        cmd_token = _strtok(input, "\n");
-        while (cmd_token && cmd_count < MAX_INPUT/2)
-        {
-            commands[cmd_count++] = cmd_token;
-            cmd_token = _strtok(NULL, "\n");
-        }
-        commands[cmd_count] = NULL;
+	/* Split into commands (by newline) */
+	cmd_count = 0;
+	cmd_token = _strtok(input, "\n");
+	while (cmd_token && cmd_count < MAX_INPUT/2)
+	{
+	commands[cmd_count++] = cmd_token;
+	cmd_token = _strtok(NULL, "\n");
+	}
+	commands[cmd_count] = NULL;
 
-        /* Process each command */
-        for (i = 0; commands[i]; i++)
-        {
-            /* Split command into arguments */
-            j = 0;
-     arg_token = _strtok(commands[i], " \t");
-            while (arg_token && j < MAX_ARGS - 1)
-            {
-                args[j++] = arg_token;
-                arg_token = _strtok(NULL, " \t");
-            }
-            args[j] = NULL;
+	/* Process each command */
+	for (i = 0; commands[i]; i++)
+	{
+	/* Split command into arguments */
+	j = 0;
+	arg_token = _strtok(commands[i], " \t");
+	while (arg_token && j < MAX_ARGS - 1)
+	{
+	args[j++] = arg_token;
+	arg_token = _strtok(NULL, " \t");
+	}
+	args[j] = NULL;
 
-            if (args[0])
-     {
-  
-    last_status = execute_command(args, &last_status, &cmd_counter, env);
-                cmd_counter++;
-     }
- 
- }
-    }
+	if (args[0])
+	{
 
-    return (last_status);
+	last_status = execute_command(args, &last_status, &cmd_counter, env);
+	cmd_counter++;
+	}
+
+	}
+	}
+
+	return (last_status);
 }
